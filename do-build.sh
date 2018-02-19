c26aa58e8741f0dfe4a55ea21fb7afd546942ab5
Markos Chandras <mchandras@suse.de>
2018-02-19 21:53:28 +0000

    do-build.sh: Add additional packages to OS images
    Install some additional packages to OS images.

--- do-build.sh
+++ do-build.sh
@@ -6,6 +6,7 @@ ONE_DISTRO=${ONE_DISTRO:-}
 
 declare -A flavors=( ["ubuntu-minimal"]="xenial" ["opensuse-minimal"]="42.3" ["centos-minimal"]="7" )
 declare -r elements="vm simple-init devuser growroot openssh-server"
+declare -r packages="vim,gdb,strace,htop,moreutils"
 declare -r one_distro=${1}
 declare -r BASE_PATH=$(dirname $(readlink -f $0) | sed "s@/xci/.*@@")
 
@@ -34,11 +35,17 @@ sudo -H pip install -q diskimage-builder==2.9.0
 do_build() {
 	local image=${1}-minimal
 	local image_name=${1}
+	local os_packages=${packages}
 	echo "Building ${image}-${flavors[$image]}..."
 	export DIB_RELEASE=${flavors[$image]}
 	# Some defaults
 	export DIB_YUM_MINIMAL_CREATE_INTERFACES=1 # centos dhcp setup
-	disk-image-create -t qcow2 -u --no-tmpfs \
+	if [[ ${image_name} == ubuntu ]]; then
+		os_packages="${packages},iputils-ping"
+	else
+		os_packages="${packages},iputils"
+	fi
+	disk-image-create -t qcow2 -u --no-tmpfs -p ${os_packages} \
 		--image-size 10G -o ${image_name}.qcow2 ${elements} $image
 	sha256sum ${image_name}.qcow2 > ${image_name}.qcow2.sha256.txt
 	echo "Done!"
